e6f886b8c1777137fc14437681afda1f1bf160e2
devbanerjee-cyber <devbanerjee@gmail.com>
2026-06-27 23:40:12 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,14 @@
+
+// ✅ Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 const firebaseConfig = {
   apiKey: "AIzaSyDourbydggTKB2T9DtWuda0HaOM0F7exm0",
   authDomain: "devfirebasegit.firebaseapp.com",
   projectId: "devfirebasegit",
   storageBucket: "devfirebasegit.appspot.com",
 
-  // ⚠️ Replace this with correct value from Firebase console
-  messagingSenderId: "121439772482",
+    messagingSenderId: "G-098FK119DM",
 
   appId: "1:121439772482:web:732787bbb283cfcf1bf2db"
 };
