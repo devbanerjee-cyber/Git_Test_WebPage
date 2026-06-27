cecf9bd4a31d0e4624536aa8572c6bcec7b05e4b
devbanerjee-cyber <devbanerjee@gmail.com>
2026-06-27 23:44:18 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,13 @@
-
-// ✅ Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// Firebase configuration for Training Portal
 
 const firebaseConfig = {
   apiKey: "AIzaSyDourbydggTKB2T9DtWuda0HaOM0F7exm0",
   authDomain: "devfirebasegit.firebaseapp.com",
   projectId: "devfirebasegit",
   storageBucket: "devfirebasegit.appspot.com",
-
-    messagingSenderId: "G-098FK119DM",
-
+  messagingSenderId: "121439772482",
   appId: "1:121439772482:web:732787bbb283cfcf1bf2db"
 };
 
-// ✅ Initialize Firebase
+// Initialize Firebase only once
 firebase.initializeApp(firebaseConfig);
